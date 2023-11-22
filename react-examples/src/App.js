960ccf3d0ca4f21e8d37068a8c7c04dfa5031c1a
roman prybula <roman.bismail@gmail.com>
2023-11-22 14:42:52 +0100

    finished 01_components

--- react-examples/src/App.js
+++ react-examples/src/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+import PaintingList from "./components/PaintingList";
+import Section from "./components/Section";
+import paintings from "./paintings.json";
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  return (
+    <div>
+      <Section title="Топ недели">
+        <PaintingList items={paintings} />
+      </Section>
+
+      <Section title="Лучшее"></Section>
+    </div>
+  );
 }
-
-export default App;
